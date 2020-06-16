d337cb74e6c580c4a31948fc7721dda5fd56a6a9
vvfar <qw121qw@163.com>
2020-06-16 16:43:55 +0800

    2020-06-16 16:00

--- application/index/controller/Sx.php
+++ application/index/controller/Sx.php
@@ -282,6 +282,10 @@ class Sx extends Controller
             $this->assign('page',$page);
             $this->assign('pagesize',15);
             $this->assign('sxs',$sxs);
+            $this->assign('s_department',$s_department);
+            $this->assign('sxMoney',$sxMoney);
+            $this->assign('syyh',$syyh);
+            $this->assign('syed',$syed);
 
             return $this->fetch();
         }else{
@@ -567,24 +571,18 @@ class Sx extends Controller
             $this->assign('status',$status);
             $this->assign('sx_line',$sx_line);
             $this->assign('newLevel',$newLevel);
-
+            $this->assign('fls',$fls);
             $this->assign('dateTime',$dateTime);
             $this->assign('date1',$date1);
             $this->assign('hkje',$hkje);
             $this->assign('wyfl',$wyfl);
             $this->assign('hkfsbz',$hkfsbz);
-
             $this->assign('hkfs',$hkfs);
             $this->assign('sjhkje',$sjhkje);
             $this->assign('hkfs2',$hkfs2);
             $this->assign('sjhkfs',$sjhkfs);
-            $this->assign('now',$now);
-
-
-            $this->assign('fls',$fls);
+            $this->assign('now',$now);        
             
-
-            //var_dump($hkje);
             return $this->fetch();
         }else{
             return $this->redirect('/index.php/Index/Login/login');
@@ -597,7 +595,92 @@ class Sx extends Controller
         if(isset($_SESSION["username"])){
             $username=$_SESSION["username"];
 
+            if(!isset($_GET["date1"]) && !isset($_GET["date1"]) && !isset($_GET["companyName"])  ){
+                $date1="";
+                $date2="";
+                $companyName="";
+            }else{
+                $date1=$_GET["date1"];
+                $date2=$_GET["date2"];
+                $companyName=$_GET["companyName"];
+            }
+            
+            $sqlstr1=\think\Db::name("user_form")->field(["department","newLevel"])->where("username",$username)->find();
+
+            $department=$sqlstr1["department"];
+            $newLevel=$sqlstr1["newLevel"];
+
+            //分页代码
+            if(!isset($_GET["page"]) || !is_numeric($_GET["page"])){
+                $page=1;
+            }else{
+                $page=intval($_GET["page"]);
+            }
+
+            $pagesize=15;
+
+            $sqlstr3="select count(*) as total from sx_form a,hk_form2 b where a.sqid=b.sqid  and b.status <> '已审核' ";
+
+            if($date1 !="" && $date2 !=""){
+                $sqlstr3= $sqlstr3." and a.date3>='$date1' and a.date3<= '$date2'";
+            }
+
+            if($companyName !=""){
+                $sqlstr3= $sqlstr3." and a.companyName like '%$companyName%'";
+            }
+
+            if($newLevel !="ADMIN" and $department != "商业运营部" and $department !="财务部"){
+                if($newLevel == "KA"){
+                    $sqlstr3=$sqlstr3." and a.ywy='$username' ";
+                }else{
+                    $sqlstr3=$sqlstr3." and '$department' like concat('%',a.department,'%') ";
+                }
+            }
+                       
+            $sqlstr3=Db::query($sqlstr3);
+
+            $total=$sqlstr3[0]['total'];
+
+            if($total%$pagesize==0){
+                $pagecount=intval($total/$pagesize);
+            }else{
+                $pagecount=ceil($total/$pagesize);
+            }
+
+            $sqlstr2="select distinct a.id,a.date1,a.sqid,a.companyName,a.department,a.ywy,a.sqmoney,". 
+                        "b.dhkje,a.status2,b.status,c.newMoney ".
+                        "from sx_form a,hk_form2 b,use_sx c where a.sqid=b.sqid and a.sqid=c.sqid and b.status <> '已审核' ";
+
+            if($date1 !="" and $date2 !=""){
+                $sqlstr2=$sqlstr2." and a.date3 >= '$date1' and a.date3 <= '$date2'";
+            }
+
+            if($companyName !=""){
+                $sqlstr2=$sqlstr2." and a.companyName like '%$companyName%'";
+            }
+            
+            if($newLevel !="ADMIN" and $department != "商业运营部" and $department !="财务部"){
+                if($newLevel == "KA"){
+                    $sqlstr2=$sqlstr2." and a.ywy='$username' ";
+                }else{
+                    $sqlstr2=$sqlstr2." and '$department' like concat('%',a.department,'%') ";
+                }
+            }
+
+            $sqlstr2=$sqlstr2." order by a.date1 desc limit ".($page-1)*$pagesize.",$pagesize";
+
+            $sxs=Db::query($sqlstr2);
+
+
+
             $this->assign('username',$username);
+            $this->assign("total",$total);
+            $this->assign('pagecount',$pagecount);
+            $this->assign('page',$page);
+            $this->assign('sxs',$sxs);
+            $this->assign('date1',$date1);
+            $this->assign('date2',$date2);
+            $this->assign('companyName',$companyName);
 
             return $this->fetch();
         }else{
@@ -611,7 +694,176 @@ class Sx extends Controller
         if(isset($_SESSION["username"])){
             $username=$_SESSION["username"];
 
+            if(!isset($_GET["date1"]) && !isset($_GET["date1"]) && !isset($_GET["companyName"])  ){
+                $date1="";
+                $date2="";
+                $companyName="";
+            }else{
+                $date1=$_GET["date1"];
+                $date2=$_GET["date2"];
+                $companyName=$_GET["companyName"];
+            }
+
+            $sqlstr1=\think\Db::name("user_form")->field(["department","newLevel"])->where("username",$username)->find();
+
+            $department=$sqlstr1["department"];
+            $newLevel=$sqlstr1["newLevel"];
+
+            //分页代码
+            if(!isset($_GET["page"]) || !is_numeric($_GET["page"])){
+                $page=1;
+            }else{
+                $page=intval($_GET["page"]);
+            }
+
+            $pagesize=15;
+
+            $sqlstr3="select count(*) as total from sx_form a,hk_form b where a.sqid=b.sqid and a.status='已生效'";
+
+            if($date1 !="" && $date2 !=""){
+                $sqlstr3= $sqlstr3." and a.date3>='$date1' and a.date3<= '$date2'";
+            }
+
+            if($companyName !=""){
+                $sqlstr3= $sqlstr3." and a.companyName like '%$companyName%'";
+            }
+
+            if($newLevel !="ADMIN"){
+                $sqlstr3=$sqlstr3." and (a.department='$department' or a.gxDepartment like '%$department%')";
+            }
+            
+            $sqlstr3=Db::query($sqlstr3);
+
+            $total=$sqlstr3[0]['total'];
+
+            if($total%$pagesize==0){
+                $pagecount=intval($total/$pagesize);
+            }else{
+                $pagecount=ceil($total/$pagesize);
+            }
+
+            $sqlstr2="select distinct a.id,a.date1,a.sqid,a.companyName,a.department,a.ywy,a.sqmoney,". 
+            "b.dhkje,a.status2,a.status,c.newMoney,a.dateTime,a.hkje,b.date2,b.sjhkje,a.date3,a.wyfl ".
+            "from sx_form a,hk_form b,use_sx c where a.sqid=b.sqid and a.sqid=c.sqid and a.status='已生效'";
+
+            if($date1 !="" and $date2 !=""){
+                $sqlstr2=$sqlstr2." and a.date3 >= '$date1' and a.date3 <= '$date2'";
+            }
+
+            if($companyName !=""){
+                $sqlstr2=$sqlstr2." and a.companyName like '%$companyName%'";
+            }
+            
+            if($newLevel !="ADMIN" and $department !="财务部"){
+                $sqlstr2=$sqlstr2." and (a.department='$department' or a.gxDepartment like '%$department%')";
+            }
+
+            $sqlstr2=$sqlstr2." order by a.date1 desc limit ".($page-1)*$pagesize.",$pagesize";
+
+            $sxs=Db::query($sqlstr2);
+
+            $myTotal=1;
+
+            $yqsj="";
+
+            for($i=0;$i<sizeof($sxs);$i++){
+                $arr_shr=explode(",",$sxs[0]["status"]);
+                $shr=array_pop($arr_shr);
+                
+                //计划回款期数，金额
+                $arr_qs=explode(",",$sxs[0]["dateTime"]);
+                $arr_hkje=explode(",",$sxs[0]["hkje"]);
+                $arr_wyfl=explode(",",$sxs[0]["wyfl"]);
+
+                //实际回款期数，金额
+                $arr_qs2=explode(",",$sxs[0]["date2"]);
+                $arr_hkje2=explode(",",$sxs[0]["sjhkje"]);
+                
+                //合同期限
+                $lastDate=$sxs[0]["date3"];
+
+                $qs=0;
+                $all_jhhk=0;
+
+                //到期时间
+                $expireDate="";
+                $yqsj="";
+
+                date_default_timezone_set("Asia/Shanghai");
+                //$date1=date('Y-m-d', time());
+
+                $date1=date('Y-m-d', time());
+                $date2=date("Y-m-d",strtotime("+1week",strtotime(date('Y-m-d', time()))));
+
+
+                for($count=0;$count<sizeof($arr_qs);$count++){
+                    if($arr_qs[$count] != ""){
+                        $qs=$qs+1;
+
+                        if($arr_qs[$count] <= $date2){
+                            $all_jhhk=$all_jhhk+$arr_hkje[$count]*(1+$arr_wyfl[$count]/100);
+                            $expireDate=$arr_qs[$count];
+                        }
+                    }
+                }
+                
+                    
+                if($qs==0){
+                    $expireDate=$lastDate;
+                    $all_jhhk=$sxs[0]["sqmoney"];
+                }
+
+                $qs2=0;
+                $all_sjhk=0;
+
+
+                for($count=0;$count<sizeof($arr_qs2);$count++){
+                    if($arr_qs2[$count] != ""){
+                        $qs2=$qs2+1;
+
+                        if($arr_qs[$count] <= $date2){
+                            $all_sjhk=$all_sjhk+$arr_hkje2[$count];
+                        }
+                    } 
+                }
+                
+                if($qs2==0){
+                    $all_sjhk=$sxs[0]["sqmoney"]-$sxs[0]["dhkje"];
+                }
+
+                
+
+                if($all_jhhk > $all_sjhk){
+                    //逾期天数
+                    $yqsj=floor((strtotime($expireDate)-strtotime($date1))/86400);
+
+                    if($yqsj<=7 and $yqsj >= 0){
+                        $yqsj=$yqsj."天到期";
+                    }else{
+                        $yqsj="";
+                    }
+                }else{
+                    $expireDate="";
+                }
+
+                if($expireDate > $date2 or $expireDate < $date1){
+                    $expireDate="";
+                }
+
+            }
+
+
             $this->assign('username',$username);
+            $this->assign('sxs',$sxs);
+            $this->assign('yqsj',$yqsj);
+            $this->assign("total",$total);
+            $this->assign('pagecount',$pagecount);
+            $this->assign('page',$page);
+            $this->assign('date1',$date1);
+            $this->assign('date2',$date2);
+            $this->assign('companyName',$companyName);
+            
+            
 
             return $this->fetch();
         }else{
@@ -625,7 +877,171 @@ class Sx extends Controller
         if(isset($_SESSION["username"])){
             $username=$_SESSION["username"];
 
+            if(!isset($_GET["date1"]) && !isset($_GET["date1"]) && !isset($_GET["companyName"])  ){
+                $date1="";
+                $date2="";
+                $companyName="";
+            }else{
+                $date1=$_GET["date1"];
+                $date2=$_GET["date2"];
+                $companyName=$_GET["companyName"];
+            }
+                        
+            $username=$_SESSION["username"];
+
+            $sqlstr1=\think\Db::name("user_form")->field(["department","newLevel"])->where("username",$username)->find();
+
+            $department=$sqlstr1["department"];
+            $newLevel=$sqlstr1["newLevel"];
+
+            //分页代码
+            if(!isset($_GET["page"]) || !is_numeric($_GET["page"])){
+                $page=1;
+            }else{
+                $page=intval($_GET["page"]);
+            }
+
+            $pagesize=15;
+
+            $sqlstr3="select count(*) as total from sx_form a,hk_form b where a.sqid=b.sqid and a.status='已生效'";
+
+            if($date1 !="" && $date2 !=""){
+                $sqlstr3= $sqlstr3." and a.date3>='$date1' and a.date3<= '$date2'";
+            }
+
+            if($companyName !=""){
+                $sqlstr3= $sqlstr3." and a.companyName like '%$companyName%'";
+            }
+
+            if($newLevel !="ADMIN" and $department !="财务部"){
+                $sqlstr3=$sqlstr3." and (a.department='$department' or a.gxDepartment like '%$department%')";
+            }
+
+            $sqlstr3=Db::query($sqlstr3);
+
+            $total=$sqlstr3[0]['total'];
+
+            if($total%$pagesize==0){
+                $pagecount=intval($total/$pagesize);
+            }else{
+                $pagecount=ceil($total/$pagesize);
+            }
+
+            $sqlstr2="select distinct a.id,a.date1,a.sqid,a.companyName,a.department,a.ywy,a.sqmoney,". 
+            "b.dhkje,a.status2,a.status,c.newMoney,a.dateTime,a.hkje,b.date2,b.sjhkje,a.date3,a.wyfl ".
+            "from sx_form a,hk_form b,use_sx c where a.sqid=b.sqid and a.sqid=c.sqid and a.status='已生效'";
+
+            if($date1 !="" and $date2 !=""){
+                $sqlstr2=$sqlstr2." and a.date3 >= '$date1' and a.date3 <= '$date2'";
+            }
+
+            if($companyName !=""){
+                $sqlstr2=$sqlstr2." and a.companyName like '%$companyName%'";
+            }
+            
+            if($newLevel !="ADMIN" and $department !="财务部"){
+                $sqlstr2=$sqlstr2." and (a.department='$department' or a.gxDepartment like '%$department%')";
+            }
+
+            //$sqlstr2=$sqlstr2." order by a.date1 desc limit ".($page-1)*$pagesize.",$pagesize";
+
+            $sqlstr2=$sqlstr2." order by a.date1 desc";
+
+            $sxs=Db::query($sqlstr2);
+
+
+            $yqsj="";
+
+            for($i=0;$i<sizeof($sxs);$i++){
+                            
+                //计划回款期数，金额
+                $arr_qs=explode(",",$sxs[0]["dateTime"]);
+                $arr_hkje=explode(",",$sxs[0]["hkje"]);
+                $arr_wyfl=explode(",",$sxs[0]["wyfl"]);
+
+                //实际回款期数，金额
+                $arr_qs2=explode(",",$sxs[0]["date2"]);
+                $arr_hkje2=explode(",",$sxs[0]["sjhkje"]);
+
+                $qs=0;
+                $all_jhhk=0;
+
+                //到期时间
+                $expireDate="";
+                $yqsj="";
+
+                //合同期限
+                $lastDate=$sxs[0]["date3"];
+
+                date_default_timezone_set("Asia/Shanghai");
+                $date1=date('Y-m-d', time());
+
+                for($count=0;$count<sizeof($arr_qs);$count++){
+                    if($arr_qs[$count] != ""){
+                        $qs=$qs+1;
+
+                        if($arr_qs[$count] <= $date1){
+                            $all_jhhk=$all_jhhk+$arr_hkje[$count]*(1+$arr_wyfl[$count]/100);
+                            $expireDate=$arr_qs[$count];
+                        }
+                    }
+                }
+
+                if($qs==0){
+                    $expireDate=$lastDate;
+                    $all_jhhk=$sxs[0]["sqmoney"];
+                }
+
+                $qs2=0;
+                $all_sjhk=0;
+
+                for($count=0;$count<sizeof($arr_qs2);$count++){
+                    if($arr_qs2[$count] != ""){
+                        $qs2=$qs2+1;
+
+                        if($arr_qs2[$count] <= $date1){
+                            $all_sjhk=$all_sjhk+$arr_hkje2[$count];
+                        }
+                    } 
+                }
+
+                if($qs2==0){
+                    $all_sjhk=$sxs[0]["sqmoney"]-$sxs[0]["dhkje"];
+                }
+                
+                if($all_jhhk > $all_sjhk){
+                    //逾期天数
+                    $yqsj=floor((strtotime($date1)-strtotime($expireDate))/86400);
+
+                    if($yqsj<=30 and $yqsj > 0){
+                        $yqsj="逾期".$yqsj."天";
+                    }elseif($yqsj > 30){
+                        $yqsj="逾期30天以上";
+                    }else{
+                        $yqsj = "";
+                    }
+                }else{
+                    $expireDate="";
+                }
+                
+                if($expireDate>$date1){
+                    $expireDate="";
+                    $all_jhhk=0;
+                }
+            }
+
+
+
             $this->assign('username',$username);
+            $this->assign("total",$total);
+            $this->assign('pagecount',$pagecount);
+            $this->assign('page',$page);
+            $this->assign('sxs',$sxs);
+            $this->assign('yqsj',$yqsj);
+            $this->assign("total",$total);
+            $this->assign('date1',$date1);
+            $this->assign('date2',$date2);
+            $this->assign('companyName',$companyName);
 
             return $this->fetch();
         }else{
@@ -654,12 +1070,69 @@ class Sx extends Controller
                 $companyName=$_GET["companyName"];
             }
 
+            //分页代码
+            if(!isset($_GET["page"]) || !is_numeric($_GET["page"])){
+                $page=1;
+            }else{
+                $page=intval($_GET["page"]);
+            }
+
+            $pagesize=15;
+
+            $sqlstr3="select count(*) as total from sx_form a,hk_form b where a.sqid=b.sqid and a.status='已作废' ";
+            
+            if($date1 !="" && $date2 !=""){
+                $sqlstr3= $sqlstr3." and a.date3>='$date1' and a.date3<= '$date2'";
+            }
+
+            if($companyName !=""){
+                $sqlstr3= $sqlstr3." and a.companyName like '%$companyName%'";
+            }
+
+            if($newLevel !="ADMIN" and $department !="财务部"){
+                $sqlstr3=$sqlstr3." and (a.department='$department' or a.gxDepartment like '%$department%')";
+            }
+            
+            $sqlstr3=Db::query($sqlstr3);
+
+            $total=$sqlstr3[0]['total'];
+
+            if($total%$pagesize==0){
+                $pagecount=intval($total/$pagesize);
+            }else{
+                $pagecount=ceil($total/$pagesize);
+            }
+
+            $sqlstr2="select distinct a.id,a.date1,a.sqid,a.companyName,a.department,a.ywy,a.sqmoney,". 
+                        "b.dhkje,a.status2,a.status,c.newMoney ".
+                        "from sx_form a,hk_form b,use_sx c where a.sqid=b.sqid and a.sqid=c.sqid and a.status='已作废' ";
+
+            if($date1 !="" and $date2 !=""){
+                $sqlstr2=$sqlstr2." and a.date3 >= '$date1' and a.date3 <= '$date2'";
+            }
+
+            if($companyName !=""){
+                $sqlstr2=$sqlstr2." and a.companyName like '%$companyName%'";
+            }
+            
+            if($newLevel !="ADMIN" and $department !="财务部"){
+                $sqlstr2=$sqlstr2." and (a.department='$department' or a.gxDepartment like '%$department%')";
+            }
+
+            $sqlstr2=$sqlstr2." order by a.date1 desc limit ".($page-1)*$pagesize.",$pagesize";
+
+            $sxs=Db::query($sqlstr2);
+
 
             $this->assign('username',$username);
             $this->assign('date1',$date1);
             $this->assign('date2',$date2);
             $this->assign('companyName',$companyName);
             $this->assign('newLevel',$newLevel);
+            $this->assign("total",$total);
+            $this->assign('pagecount',$pagecount);
+            $this->assign('page',$page);
+            $this->assign('sxs',$sxs);
 
             return $this->fetch();
         }else{
